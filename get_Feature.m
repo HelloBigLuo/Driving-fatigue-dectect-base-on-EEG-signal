c873051c286dc06f30f501d486c848f5134677cb
HelloBigLuo <you@example.com>
2020-06-22 22:47:15 +0800

    second commit

--- get_Feature.m
+++ get_Feature.m
@@ -1,21 +1,27 @@
 function [feature] = get_Feature(data,trail)
+%get_Feature()函数获取数据段特征
+%input： 
+%       data：代处理，代分段的数据
+%       trail：划分的trail时间范围,N*2
+%output: 
+%       feature：该数据提取的特征，每一个trail段可以
+%             提取30个特征，N个trial，为N*30
 
-feature = [];
+feature = [];   
 
-len = trail(1,2)-trail(1,1);
+len = trail(1,2)-trail(1,1);%数据段长度，固定
 window=boxcar(len);
-Nfft=2^ceil(log2(len));
-noverlap = len/2;
-fs = 250; 
+Nfft=2^ceil(log2(len)); %fft的点数，2的整数幂
+fs = 250;   %采样频率
 
-for i=1:length(trail) 
+for i=1:length(trail)  %每个trail段
     averange = [];
-    for j = 1:30
-        xn = data(j,trail(i,1):trail(i,2));
-        [Pxx f]=pwelch(xn,window,[],Nfft,fs);
-        Pxxx = 10*log10(Pxx);
-        index = find(f>4&f<7);
-        averange = [averange,mean(Pxxx(index))];
+    for j = 1:30  %每个通道
+        xn = data(j,trail(i,1):trail(i,2)); %数据段
+        [Pxx f]=pwelch(xn,window,[],Nfft,fs);%提取功率谱
+        Pxxx = 10*log10(Pxx);   %功率谱换成db
+        index = find(f>4&f<7);  
+        averange = [averange,mean(Pxxx(index))];%4-7Hz平均功率
     end
     feature = [feature;averange];
 end
--- get_Trail.m
+++ get_Trail.m
@@ -1,14 +1,22 @@
 function [result,trail] = get_Trail(data,idsNAN)
+%get_Trail()函数完成滤波与分段
+%input：
+%       data：代处理，代分段的数据
+%       idsNAN：数据段是否有效
+%output:
+%       result：已滤波的数据段
+%       trail：划分的trail时间范围，N*[起始数据标号，最终数据标号]，
+%           代表N个trail的时间范围。
 
-fc = 250;
-wn = [1*2 50*2]/fc;                  %1-50HZl滤波
-[k,l] = butter (2,wn);
+fc = 250;                   %采样频率
+wn = [1*2 50*2]/fc;         %1-50HZl滤波
+[k,l] = butter (2,wn);      %巴特沃斯滤波器，4阶滤波器
 result = filtfilt(k,l,double(data));
 trail = [];
 
-len = length(idsNAN)-1
+len = length(idsNAN)-1;
 for i=0:len
-    if idsNAN(i+1,1)==0
+    if idsNAN(i+1,1)==0     %数据有效，则添加分段
         trail = [trail;i*10*fc+1,(i*10+30)*fc];
     end
 end
